0437d76c4e1f1f0d13f4af49cb4489e1bf3aefba
Zahran Rafif <zahranlumia@gmail.com>
2023-05-12 00:34:30 +0700

    Halaman kategori barang

---
+++ lib/view/barang/kategoriBarang/kategori_barang.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_api/controller/kategori_barang_controller.dart';
+import 'package:flutter_api/model/kategori_barang_model.dart';
+import 'package:flutter_api/view/barang/kategoriBarang/add_kategori_barang.dart';
+
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  List<KategoriBarangModel> listKategoriBarang = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void getKategoriBarang() async {
+    final kategoriBarang = await kategoriBarangController.getKategoriBarang();
+    setState(() {
+      listKategoriBarang = kategoriBarang;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kategori Barang'),
+      ),
+      body: SafeArea(
+          child: ListView.builder(
+        itemCount: listKategoriBarang.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(listKategoriBarang[index].nama),
+              trailing:
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+            ),
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddKategoriBarang()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
