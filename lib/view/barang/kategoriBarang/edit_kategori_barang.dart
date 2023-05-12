c339c1526880205a4b87c60e01be7a7e5f990cf0
Zahran Rafif <zahranlumia@gmail.com>
2023-05-12 21:30:31 +0700

    Membuat halaman edit kategori barang

---
+++ lib/view/barang/kategoriBarang/edit_kategori_barang.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditKategoriBarang extends StatefulWidget {
+  const EditKategoriBarang({super.key});
+
+  @override
+  State<EditKategoriBarang> createState() => _EditKategoriBarangState();
+}
+
+class _EditKategoriBarangState extends State<EditKategoriBarang> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
