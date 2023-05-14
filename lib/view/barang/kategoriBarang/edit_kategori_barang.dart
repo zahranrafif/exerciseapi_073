7767c95e56e6002e75da3af894114147c7017b1c
Zahran Rafif <zahranlumia@gmail.com>
2023-05-14 23:22:55 +0700

    Lanjutan membuat halaman edit kategori barang

--- lib/view/barang/kategoriBarang/edit_kategori_barang.dart
+++ lib/view/barang/kategoriBarang/edit_kategori_barang.dart
@@ -12,6 +12,37 @@ class EditKategoriBarang extends StatefulWidget {
 class _EditKategoriBarangState extends State<EditKategoriBarang> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Halaman Edit Kategori Barang'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nama Kategori Barang',
+                labelText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori is Required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Update'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
