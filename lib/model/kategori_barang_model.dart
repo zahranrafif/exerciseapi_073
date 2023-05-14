f695832ccf3dae887c3f85d3dcf6399335c1b16c
Zahran Rafif <zahranlumia@gmail.com>
2023-05-14 23:07:14 +0700

    Menambah variabel Id di Model

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,15 +2,19 @@ import 'dart:convert';
 
 class KategoriBarangModel {
   final String nama;
+  var id;
   KategoriBarangModel({
     required this.nama,
+    this.id,
   });
 
   KategoriBarangModel copyWith({
     String? nama,
+    int? id,
   }) {
     return KategoriBarangModel(
       nama: nama ?? this.nama,
+      id: id ?? this.id,
     );
   }
 
@@ -18,6 +22,7 @@ class KategoriBarangModel {
   Map<String, dynamic> toMap() {
     return {
       'nama_kategori_barang': nama,
+      'id_kategori_barang': id,
     };
   }
 
@@ -25,24 +30,25 @@ class KategoriBarangModel {
   factory KategoriBarangModel.fromMap(Map<String, dynamic> map) {
     return KategoriBarangModel(
       nama: map['nama_kategori_barang'] ?? '',
+      id: map['id_kategori_barang']?.toInt() ?? 0,
     );
   }
 
   String toJson() => json.encode(toMap());
 
   factory KategoriBarangModel.fromJson(String source) =>
-      KategoriBarangModel.fromMap(json.decode(source));
+      KategoriBarangModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'KategoriBarangModel(nama: $nama)';
+  String toString() => 'KategoriBarangModel(nama: $nama, id: $id)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is KategoriBarangModel && other.nama == nama;
+    return other is KategoriBarangModel && other.nama == nama && other.id == id;
   }
 
   @override
-  int get hashCode => nama.hashCode;
+  int get hashCode => nama.hashCode ^ id.hashCode;
 }
